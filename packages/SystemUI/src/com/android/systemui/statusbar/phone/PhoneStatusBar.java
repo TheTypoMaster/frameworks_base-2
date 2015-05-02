2986efb721f84f10be4201dde9d6babdd487daa5
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2015-05-02 17:09:57 +0200

    [2/2] frameworks_base: Add options for battery icon and text in status bar
    Available options :
    
    - icon : portrait, landscape battery / normal, dotted circle / text only / none
    - battery percent : none / inside icon / next to icon
    
    Charging with portrait and landscape icons :
    
    - without battery percent inside --> show a bolt inside of icon blinking
    - with battery percent inside --> alternate every second between battery percent and bolt inside of icon
    
    Charging with circle icon :
    
    - without battery percent inside --> charged subset of circle will rotate with bolt inside blinking
    - with battery percent inside --> charged subset of circle will rotate with bolt and battery percent alternating inside
    
    Charging text only :
    
    - will show 85~ instead of 85% when charging
    
    To do for v1 :
    
    - nothing, can be merged from my point of view
    
    To do for v2 :
    
    - animate battery percent for text only style
    - rewrite battery drawing code to properly partially invert text/bolt
    
    Based on Zavon Joseph patchset 5
    
    Extended with text option by Jean-Pierre Rasquin <yank555.lu@gmail.com> :
    
    - fixed text not showing on lockscreen at all
    
    - fixed always displaying text when charging on lockscreen bug, now respects user setting on lockscreen as well
    
    - fixed text not showing on expanded notifications
    
    - fixed text only won't stick, will show portrait battery plus text on reboot, settings will reflect last user setting, and be different to what is actually applied
    
    - fixed text will always show on expanded notifications, even when quick tiles are not showing
    
    - fixed white on white landscape in icon battery text (text will switch color as soon as 25% is reached)
    
    - added only show text next to icon in expanded notifications when percent is not shown in the icon, else it's redundant
    
    - fixed code layout
    
    - added charging animation for portrait / landscape icon when battery percent is set to 'inside icon'
    
    - fixed charging bolt not correctly showing on landscape icon
    
    - added charging animation (battery percent - bolt) to circle battery icon
    
    - fixed my derp that never showed charge bolt when battery percent was set to hidden or next to icon
    
    - added animate bolt even when battery percent set to hidden
    
    - fixed charged part of battery icon not drawn between hardcoded critical level (15) and battery safe mode level
    
    - fixed battery percent not shown when not opaque (low battery, white color)
    
    - maxed out circle battery icon size (thx to Griffin Millender)
    
    - added dotted battery icon style (thx to Griffin Millender)
    
    - removed two (make that three) unnecessary observers (thx to Griffin Millender)
    
    - fixed the missing update to notification battery icon (thx to Griffin Millender)
    
    - tiny code cleanup in BatteryMeterView.java
    
    - removed BatteryLevelTextView class, this is not needed
    
    - removed one more unnecessary observer (in BatteryLevelTextView)
    
    - added updating battery level text from PhoneStatusBar to StatusBarHeaderView and KeyguardStatusBarView  to avoid the user actually seeing the update when pulling down notifications and battery has dropped since last time notifications were pulled and getting to lockscreen (unlikely, but this way it's settled for good)
    
    - added battery style text only charging feedback (preliminary, will show 85~ instead of 85%)
    
    Change-Id: I6b6eff25759740b9288c54522e0b7909ea51db8c
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1,6 +1,4 @@
 /*
- * Copyright (c) 2012-2014 The Linux Foundation. All rights reserved.
- * Not a Contribution.
  * Copyright (C) 2010 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -68,6 +66,7 @@ import android.media.session.MediaController;
 import android.media.session.MediaSession;
 import android.media.session.MediaSessionManager;
 import android.media.session.PlaybackState;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
@@ -127,6 +126,7 @@ import com.android.internal.statusbar.StatusBarIcon;
 import com.android.keyguard.KeyguardHostView.OnDismissAction;
 import com.android.keyguard.ViewMediatorCallback;
 import com.android.systemui.BatteryMeterView;
+import com.android.systemui.BatteryMeterView.BatteryMeterMode;
 import com.android.systemui.DemoMode;
 import com.android.systemui.EventLogTags;
 import com.android.systemui.FontSizeUtils;
@@ -259,6 +259,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     BluetoothControllerImpl mBluetoothController;
     SecurityControllerImpl mSecurityController;
     BatteryController mBatteryController;
+    private BatteryMeterView mBatteryView;
+    private TextView mBatteryLevel;
     LocationControllerImpl mLocationController;
     NetworkControllerImpl mNetworkController;
     HotspotControllerImpl mHotspotController;
@@ -388,6 +390,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     int mLinger;
     int mInitialTouchX;
     int mInitialTouchY;
+    private boolean mShowBatteryText;
+    private boolean mShowBatteryTextCharging;
+    private boolean mBatteryIsCharging;
+    private int mBatteryChargeLevel;
 
     // for disabling the status bar
     int mDisabled = 0;
@@ -429,11 +435,25 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.SCREEN_BRIGHTNESS_MODE),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_BATTERY_STYLE),
+                    false, this, UserHandle.USER_ALL);
             update();
         }
 
         @Override
-        public void onChange(boolean selfChange) {
+        public void onChange(boolean selfChange, Uri uri) {
+            if (uri.equals(Settings.System.getUriFor(
+                            Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT)) ||
+                            uri.equals(Settings.System.getUriFor(
+                            Settings.System.STATUS_BAR_BATTERY_STYLE))) {
+                        mBatteryView.updateBatteryIconSettings();
+                        mHeader.updateBatteryIconSettings();
+                        mKeyguardStatusBar.updateBatteryIconSettings();
+            }
             update();
         }
 
@@ -445,9 +465,46 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             mBrightnessControl = !autoBrightness && Settings.System.getIntForUser(
                     resolver, Settings.System.STATUS_BAR_BRIGHTNESS_CONTROL,
                     0, UserHandle.USER_CURRENT) == 1;
+            loadShowBatteryTextSetting();
+            updateBatteryLevelText();
+            mBatteryLevel.setVisibility(mShowBatteryText ? View.VISIBLE : View.GONE);
         }
     }
 
+    private void loadShowBatteryTextSetting() {
+        ContentResolver resolver = mContext.getContentResolver();
+        mShowBatteryText = Settings.System.getInt(resolver,
+                Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT, 0) == 2;
+        int batteryStyle = Settings.System.getInt(resolver,
+                Settings.System.STATUS_BAR_BATTERY_STYLE, 0);
+        switch (batteryStyle) {
+            case 4:
+                //meterMode = BatteryMeterMode.BATTERY_METER_GONE;
+                mShowBatteryText = false;
+                mShowBatteryTextCharging = false;
+                break;
+
+            case 6:
+                //meterMode = BatteryMeterMode.BATTERY_METER_TEXT;
+                mShowBatteryText = true;
+                mShowBatteryTextCharging = true;
+                break;
+
+            default:
+                mShowBatteryTextCharging = false;
+                break;
+        }
+    }
+
+    private void updateBatteryLevelText() {
+        if (mBatteryIsCharging & mShowBatteryTextCharging) {
+            mBatteryLevel.setText(mContext.getResources().getString(
+                    R.string.battery_level_template_charging, mBatteryChargeLevel));
+        } else {
+            mBatteryLevel.setText(mContext.getResources().getString(
+                    R.string.battery_level_template, mBatteryChargeLevel));
+        }
+    }
 
     // ensure quick settings is disabled until the current user makes it through the setup wizard
     private boolean mUserSetup = false;
@@ -901,6 +958,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mHandlerThread = new HandlerThread(TAG, Process.THREAD_PRIORITY_BACKGROUND);
         mHandlerThread.start();
 
+        mBatteryLevel = (TextView) mStatusBarView.findViewById(
+                                                           R.id.battery_level_text);
+
         // Other icons
         mLocationController = new LocationControllerImpl(mContext); // will post a notification
         mBatteryController = new BatteryController(mContext);
@@ -914,7 +974,12 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             }
             @Override
             public void onBatteryLevelChanged(int level, boolean pluggedIn, boolean charging) {
-                // noop
+                mBatteryIsCharging = charging;
+                mBatteryChargeLevel = level;
+                loadShowBatteryTextSetting();
+                updateBatteryLevelText();
+                mHeader.updateBatteryLevel(level, charging);
+                mKeyguardStatusBar.updateBatteryLevel(level, charging);
             }
         });
         mHotspotController = new HotspotControllerImpl(mContext);
@@ -1084,8 +1149,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mUserInfoController.reloadUserInfo();
 
         mHeader.setBatteryController(mBatteryController);
-        ((BatteryMeterView) mStatusBarView.findViewById(R.id.battery)).setBatteryController(
-                mBatteryController);
+        mBatteryView = (BatteryMeterView) mStatusBarView.findViewById(R.id.battery);
+        mBatteryView.setBatteryController(mBatteryController);
         mKeyguardStatusBar.setBatteryController(mBatteryController);
         mHeader.setNextAlarmController(mNextAlarmController);
 
@@ -3497,6 +3562,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         updateShowSearchHoldoff();
         updateRowStates();
         mScreenPinningRequest.onConfigurationChanged();
+
+        FontSizeUtils.updateFontSize(mBatteryLevel, R.dimen.battery_level_text_size);
     }
 
     @Override
