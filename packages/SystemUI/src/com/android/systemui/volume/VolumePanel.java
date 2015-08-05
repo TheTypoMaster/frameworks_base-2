SlimRoms/frameworks_base

Allow disabling adjusting volume sounds [1/2]
Conflicts:
	core/java/android/provider/Settings.java

Change-Id: I3d5bdb85e3df508d256657355001ba4270354a79

packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
@@ -1227,6 +1227,11 @@ public class VolumePanel extends Handler implements DemoMode {
     }
 
     protected void onPlaySound(int streamType, int flags) {
+        // Just return if we disabled slider sound while adjusting volume
+        if (Settings.System.getInt(mContext.getContentResolver(),
+                 Settings.System.VOLUME_ADJUST_SOUND_ENABLED, 1) == 0) {
+             return;
+        }
 
         if (hasMessages(MSG_STOP_SOUNDS)) {
             removeMessages(MSG_STOP_SOUNDS);

TheTypoMaster/frameworks_base-2

Change-Id: I2d167c2f01d20b128010197a05426618ae8b01b2

packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
@@ -1300,6 +1300,11 @@ public class VolumePanel extends Handler implements DemoMode {
     }
 
     protected void onPlaySound(int streamType, int flags) {
+        // Just return if we disabled slider sound while adjusting volume
+        if (Settings.System.getInt(mContext.getContentResolver(),
+                 Settings.System.VOLUME_ADJUST_SOUND, 1) == 0) {
+             return;
+        }
 
         if (hasMessages(MSG_STOP_SOUNDS)) {
             removeMessages(MSG_STOP_SOUNDS);